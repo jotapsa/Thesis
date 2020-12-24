15269f87a30d81e6a6b2b5c82544e39b3c9840a5
João Sá <up201506252@fe.up.pt>
2020-12-24 12:20:36 +0000

    LTE information

--- MyApplication/app/src/main/java/com/example/myapplication/Fragments/ScanFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Fragments/ScanFragment.java
@@ -1,17 +1,24 @@
 package com.example.myapplication.Fragments;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.telephony.CellIdentityLte;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoLte;
+import android.telephony.CellSignalStrengthLte;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,6 +32,7 @@ import com.example.myapplication.R;
 import java.time.Instant;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 import static android.content.Context.TELEPHONY_SERVICE;
 import static android.content.Context.WIFI_SERVICE;
@@ -35,7 +43,9 @@ import static android.content.Context.WIFI_SERVICE;
  * create an instance of this fragment.
  */
 public class ScanFragment extends Fragment {
-    private static final String TAG="ScanFragment";
+    private static final String TAG = "ScanFragment";
+
+    static final int PCI_UNAVAILABLE = 2147483647;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -91,24 +101,64 @@ public class ScanFragment extends Fragment {
         Button telephonyScanButton = view.findViewById(R.id.telephony_scan_button);
 
         TextView wifiScanText = view.findViewById(R.id.wifi_scan_text);
-        TextView telephonyScanText =  view.findViewById(R.id.telephony_scan_text);
+        TextView telephonyScanText = view.findViewById(R.id.telephony_scan_text);
 
-        WifiManager wifiManager = (WifiManager) getContext().getSystemService(WIFI_SERVICE);
+        WifiManager wifiManager = (WifiManager) Objects.requireNonNull(getContext()).getSystemService(WIFI_SERVICE);
+        StringBuilder wifiScanStringBuilder = new StringBuilder();
+
         TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(TELEPHONY_SERVICE);
+        StringBuilder telephonyScanStringBuilder = new StringBuilder();
 
         wifiScanButton.setOnClickListener(v -> {
             boolean success = wifiManager.startScan();
             if (!success) {
                 // scan failure handling
-                Log.d(TAG,"Couldn't initiate wifi scan");
+                Log.d(TAG, "Couldn't initiate wifi scan");
 //                    scanFailure();
             }
         });
 
         telephonyScanButton.setOnClickListener(v -> {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                List<CellInfo> allCellInfo = telephonyManager.getAllCellInfo();
+                telephonyScanStringBuilder.append("Scan result:\n");
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    String timestamp = String.valueOf(Instant.now().getEpochSecond());
+                    telephonyScanStringBuilder.append("Timestamp:").append(timestamp).append("\n");
+                }
+                if (allCellInfo != null) {
+                    for (CellInfo cellInfo : allCellInfo) {
+                        if (cellInfo instanceof CellInfoLte) {
+                            CellInfoLte cellInfoLte = (CellInfoLte) cellInfo;
+                            CellSignalStrengthLte signalStrengthLte = cellInfoLte.getCellSignalStrength();
+
+//                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//                                telephonyScanStringBuilder.append("RSSi:").append(signalStrengthLte.getRssi()).append(" dBm\n");
+//                            }
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                telephonyScanStringBuilder.append("RSRP:").append(signalStrengthLte.getRsrp()).append(" dBm\n");
+                                telephonyScanStringBuilder.append("RSRQ:").append(signalStrengthLte.getRsrq()).append(" dBm\n");
+                            } else {
+                                telephonyScanStringBuilder.append("dBm:").append(signalStrengthLte.getDbm()).append(" dBm\n");
+                            }
+
+                            CellIdentityLte identityLte = cellInfoLte.getCellIdentity();
+                            if (identityLte != null) {
+                                int pci = identityLte.getPci();
+                                if (pci == PCI_UNAVAILABLE) {
+                                    pci = -1;
+                                }
+                                telephonyScanStringBuilder.append("PCI:").append(pci).append("\n");
+                            }
+                        }
+                    }
+                }
+                telephonyScanText.setText(telephonyScanStringBuilder.toString());
+
+            }
+
         });
 
-        StringBuilder wifiScanStringBuilder = new StringBuilder();
         BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
 
             @RequiresApi(api = Build.VERSION_CODES.O)
@@ -120,15 +170,20 @@ public class ScanFragment extends Fragment {
                         WifiManager.EXTRA_RESULTS_UPDATED, false);
                 if (success) {
                     Log.d("BroadcastReceiver","Scan success");
-                    String timestamp = String.valueOf(Instant.now().getEpochSecond());
                     List<ScanResult> scanResultList = wifiManager.getScanResults();
-                    wifiScanStringBuilder.append("Timestamp: ").append(timestamp).append("\n");
                     wifiScanStringBuilder.append("Scan result:\n");
-                    for (ScanResult scanResult : scanResultList) {
-                        String wifiScanString = String.format(Locale.US,
-                                "SSID - %s;\nRSSi - %d dBm;\n", scanResult.SSID, scanResult.level);
-                        wifiScanStringBuilder.append(wifiScanString);
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        String timestamp = String.valueOf(Instant.now().getEpochSecond());
+                        wifiScanStringBuilder.append("Timestamp:").append(timestamp).append("\n");
                     }
+                    if (scanResultList != null) {
+                        for (ScanResult scanResult : scanResultList) {
+                            String wifiScanString = String.format(Locale.US,
+                                    "SSID - %s;\nRSSi - %d dBm;\n", scanResult.SSID, scanResult.level);
+                            wifiScanStringBuilder.append(wifiScanString);
+                        }
+                    }
+
                     wifiScanText.setText(wifiScanStringBuilder.toString());
                 } else {
                     // scan failure handling
@@ -142,5 +197,10 @@ public class ScanFragment extends Fragment {
         IntentFilter intentFilter  = new IntentFilter();
         intentFilter .addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         getContext().registerReceiver(wifiScanReceiver,  intentFilter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
